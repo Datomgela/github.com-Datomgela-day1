9ac72e21c7c6f68baeb5a5223fd4f7047c06ac19
Datomgela <138392124+Datomgela@users.noreply.github.com>
2023-07-14 15:12:52 +0400

    Add files via upload

---
+++ day 3/lesson 4(fake).py
@@ -0,0 +1,79 @@
+Banana = 45
+
+Apple = 98
+
+orange = 57
+
+print(Banana + Apple + orange)
+name = "Dato" # string-str
+
+age = 15 # integer-int
+
+height = 177.7 # float
+
+knows_programming = True # boolean-bool
+#name_knows_programming = snake_case-Python
+#nameKnowsProgramming = camelCase-JavaScript
+
+print(name)
+print(age)
+print(height)
+print(knows_programming)
+
+country -> "Germany"
+country = "Georgia"
+print(country)
+p r i n t (name)
+print(Banana))
+
+lion "animal"
+page = 156
+print(lion +)
+level = 50
+level up = 100
+print(level up)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
